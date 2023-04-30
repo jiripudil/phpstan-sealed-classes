f885e26ae5c6f9c5361460fc892a7eaba8185b63
Jiří Pudil <me@jiripudil.cz>
2023-04-30 22:53:11 +0200

    chore: replace phpstan's deprecated APIs

--- src/SealedClassUtils.php
+++ src/SealedClassUtils.php
@@ -31,20 +31,20 @@ final class SealedClassUtils
 		}
 
 		$permitsType = $scope->getType($sealed[0] ?? $sealed['permits']);
-
-		if ( ! $permitsType instanceof ConstantArrayType) {
-			return [];
-		}
-
 		$permittedClassNames = [];
-		foreach ($permitsType->getValueTypes() as $valueType) {
-			if ( ! $valueType instanceof ConstantStringType || ! $valueType->isClassString()) {
-				continue;
-			}
 
-			/** @var class-string $value */
-			$value = $valueType->getValue();
-			$permittedClassNames[] = $value;
+		foreach ($permitsType->getConstantArrays() as $constantArray) {
+			foreach ($constantArray->getValueTypes() as $valueType) {
+				foreach ($valueType->getConstantStrings() as $constantString) {
+					if ( ! $constantString->isClassStringType()->yes()) {
+						continue;
+					}
+
+					/** @var class-string $value */
+					$value = $constantString->getValue();
+					$permittedClassNames[] = $value;
+				}
+			}
 		}
 
 		return $permittedClassNames;
